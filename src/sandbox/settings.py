614ed924ff8c27d21195d5f6812e6675cfca3e2f
fahadshaikh09 <fahad_shaikh09@yahoo.co.in>
2016-12-22 00:06:14 +0530

    removed allauth-bootstrap

--- src/sandbox/settings.py
+++ src/sandbox/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'allauth_bootstrap',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
