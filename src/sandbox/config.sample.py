431bfe75af2b82f7fa4656bbb7ac7e7b51232183
fahadshaikh09 <fahad_shaikh09@yahoo.co.in>
2016-12-23 22:55:35 +0530

    sample config files uploaded

--- src/sandbox/config.sample.py
+++ src/sandbox/config.sample.py
@@ -1,6 +1,6 @@
 #email configuration
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='yourgmail@gmail.com'
-PASSWORD='yourpassword'
+EMAIL_HOST_USER='abc@gmail.com'
+EMAIL_HOST_PASSWORD='xyz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
