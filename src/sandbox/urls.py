2f883b4edf0f7535635cd63b4b97310311459aff
fahadshaikh09 <fahad_shaikh09@yahoo.co.in>
2016-12-30 12:39:05 +0530

    removed test_email url

--- src/sandbox/urls.py
+++ src/sandbox/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from workshop.views import mail_test
 from venueapp.views import oauth2callback
 import allauth
 from django.views.generic import TemplateView
@@ -25,7 +24,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='about.html')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^email_test/', mail_test),
     url(r'^profile/', include("profileapp.urls", namespace='profile')),
     url(r'^venue/', include("venueapp.urls", namespace='venue')),
     url(r'^event/', include("workshop.urls", namespace='workshop')),
